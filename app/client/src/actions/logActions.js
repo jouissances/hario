b25d66bd8aaf37aa3b3c5dda4af4e719a1716d7e
github username <github email address>
2019-05-17 21:19:02 +1000

    Move API calls to redux action file

--- app/client/src/actions/logActions.js
+++ app/client/src/actions/logActions.js
@@ -1,3 +1,47 @@
-export const logData = () => {
+let results;
 
+export function fetchResults(newLog) {
+    return (dispatch) => {
+        fetch(`http://localhost:3001/api/logs/`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newLog)
+        })
+        .then(response => response.json())
+        .then(data => {
+            // debugger
+            showResults(data['id'])
+            dispatch(fetchResultsSuccess(results))
+            // debugger
+        })
+        // debugger
+        .catch(error => dispatch(fetchResultsFailure(error)))
+    }
 }
+
+function showResults(id) {
+    // if (id !== undefined) {
+        fetch(`http://localhost:3001/api/logs/${id}`)
+        .then(response => response.json())
+        .then(data => results = data['results'])
+        .catch(error => console.log(error))
+        // debugger
+    // }
+    return results
+}
+
+export const FETCH_RESULTS_SUCCESS = 'FETCH_RESULTS_SUCCESS';
+export const FETCH_RESULTS_FAILURE = 'FETCH_RESULTS_FAILURE';
+
+export const fetchResultsSuccess = results => ({
+    type: FETCH_RESULTS_SUCCESS,
+    payload: { results }
+})
+
+export const fetchResultsFailure = error => ({
+    type: FETCH_RESULTS_FAILURE,
+    payload: { error }
+})
